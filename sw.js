8741991f5890e631692fd43abf0a5f80d6f69941
jcvelascojaztom <juancarlos@uvo.edu.mx>
2026-04-17 21:00:04 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+var CACHE = 'casio-v1';
+var ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  'https://fonts.googleapis.com/css?family=Orbitron:400,700,900|Share+Tech+Mono'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE; })
+            .map(function(k)   { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  if (e.request.url.indexOf('api.open-meteo.com') !== -1) return;
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request).then(function(response) {
+        var clone = response.clone();
+        caches.open(CACHE).then(function(cache) {
+          cache.put(e.request, clone);
+        });
+        return response;
+      });
+    })
+  );
+});
